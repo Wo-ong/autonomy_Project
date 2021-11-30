a2d5379cfcc7c359fdea785f8304cf23d44966cb
jeonghun7898 <ljh7898@gmail.com>
2021-11-30 21:01:09 +0900

    11/30

--- Autonomy_Project/webservice/go.py
+++ Autonomy_Project/webservice/go.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request
-import pickle
 import numpy as np
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
 @app.route('/select')
-def select():
+def age():
     return render_template('ab.html')
 
 @app.route('/select2')
@@ -20,10 +19,6 @@ def select2():
 @app.route('/select3')
 def select3():
     return render_template('ab_3.html')
-
-@app.route('/select3_60')
-def select3_60():
-    return render_template('ab_3_60.html')
 
 @app.route('/wait')
 def wait():
@@ -44,6 +39,30 @@ def input60():
 @app.route('/predict_ex')
 def predict_ex():
     return render_template('predict_ex.html')
+
+@app.route('/predict', methods=['POST'])
+def exercise_10():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    data9 = request.form['i']
+    data10 = request.form['j']
+    data11 = request.form['k']
+    data12 = request.form['l']
+    data13 = request.form['m']
+    data14 = request.form['n']
+
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14]])
+    pred1 = ''.join(model_10_warmup.predict(arr))
+    pred2 = ''.join(model_10_main.predict(arr))
+    pred3 = ''.join(model_10_finish.predict(arr))
+    return render_template('predict.html', res1 = pred1.split(':')[-1], res2 = ', '.join((pred2.split(':')[-1]).split(',')[:3]), res3 = pred3.split(':')[-1])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Autonomy_Project/webservice/models.py
@@ -0,0 +1,3 @@
+from go import exercise_10
+
+print(exercise_10.data1)
